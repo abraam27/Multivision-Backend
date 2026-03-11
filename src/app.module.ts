9af810fa3cea7a93c6dd3bce5fd7c3dc4d33602e
Abraam <abraamaj27@gmail.com>
2026-03-11 13:57:26 +0200

    Add @nestjs/config dependency and configure global settings
    - Added @nestjs/config version 4.0.3 to package.json and package-lock.json.
    - Configured ConfigModule in AppModule to load environment variables globally from a .env file.

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,17 @@ import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TagsModule } from './tags/tags.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
     PostsModule,
     AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
     MongooseModule.forRoot(
       'mongodb+srv://abraam27:gVwRxwsSB5nZEWiL@nestjs.ecsj6yf.mongodb.net/',
       {
