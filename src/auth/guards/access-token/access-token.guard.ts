fe543d79c22540ccc72158a3a69b2faf66981e73
Abram Emad <abram.emad@genwin.net>
2026-03-15 00:58:05 +0200

    Add AccessTokenGuard for JWT authentication and user request handling
    - Introduced AccessTokenGuard to validate JWT tokens in requests.
    - Integrated REQUEST_USER_KEY constant for user data extraction from requests.
    - Updated UsersController to use AccessTokenGuard for user creation endpoint.
    - Configured JwtModule and added necessary imports in UsersModule for JWT support.

---
+++ src/auth/guards/access-token/access-token.guard.ts
@@ -0,0 +1,40 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { REQUEST_USER_KEY } from 'src/auth/constants/auth.constants';
+
+@Injectable()
+export class AccessTokenGuard implements CanActivate {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) { }
+  async canActivate(
+    context: ExecutionContext,
+  ): Promise<boolean> {
+    // Extract the Request object from the context
+    const request: Request = context.switchToHttp().getRequest();
+
+    // Extract the token from the request headers
+    const token = request.headers.authorization?.split(' ')[1];
+    if (!token) {
+      throw new UnauthorizedException('Token is required');
+    }
+
+    try {
+      const secret = this.configService.get<string>('jwt.secret');
+      const payload: { id: string, email: string } = await this.jwtService.verifyAsync(token, {
+        secret,
+      });
+      request[REQUEST_USER_KEY] = payload;
+      console.log(request[REQUEST_USER_KEY], payload);
+    } catch (error) {
+      console.log((error as Error).message);
+      throw new UnauthorizedException('Invalid token', {
+        description: 'Error verifying token',
+      });
+    }
+    return true;
+  }
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -6,12 +6,17 @@ import { User, UserSchema } from './user.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CreateUserProvider } from './providers/create-user.provider';
 import { FindUserByEmailProvider } from './providers/find-user-by-email.provider';
+import { ConfigModule } from '@nestjs/config';
+import jwtConfig from 'src/config/jwt.config';
+import { JwtModule } from '@nestjs/jwt';
 @Module({
   controllers: [UsersController],
   providers: [UsersService, CreateUserProvider, FindUserByEmailProvider],
   imports: [
     forwardRef(() => AuthModule),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    ConfigModule.forFeature(jwtConfig),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
   ],
   exports: [UsersService],
 })
